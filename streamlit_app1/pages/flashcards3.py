d753bea0b18806499cb183352e56212e2a7a1fad
tonton-golio <aggolles97@gmail.com>
2022-09-04 18:19:00 +0200

    aaaa

--- streamlit_app1/pages/flashcards3.py
+++ streamlit_app1/pages/flashcards3.py
@@ -2,7 +2,22 @@ import streamlit as st
 import pandas as pd
 import gspread
 
-sa = gspread.service_account()
+
+credentials = {
+    "type": "service_account",
+    "project_id": "compphysics",
+    "private_key_id": st.secrets['private_key_id'],
+    "private_key": st.secrets['private_key'],
+    "client_email": st.secrets['client_email'],
+  	"client_id": st.secrets['client_id'],
+  	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  	"token_uri": "https://oauth2.googleapis.com/token",
+  	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  	"client_x509_cert_url": st.secrets['client_x509_cert_url']
+}
+
+
+sa = gspread.service_account_from_dict(credentials)
 sh = sa.open('reviewQuestions')
 
 wks = sh.worksheet('Sheet1')
@@ -17,6 +32,3 @@ records = wks.get_all_records()
 df = pd.DataFrame.from_dict(records)
 df
 #records
-
-
-wks.update('A5', 'Anton')
