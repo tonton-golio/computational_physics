50d9063e301cc6185cf27d2ff1c63f61a8665257
tonton-golio <aggolles97@gmail.com>
2022-10-13 23:02:26 +0200

    network added

--- pages/complex.py
+++ pages/complex.py
@@ -6,7 +6,12 @@ import pandas as pd
 import re
 import seaborn as sns
 from matplotlib.gridspec import GridSpec
-
+from matplotlib import pyplot as plt
+from celluloid import Camera
+import numpy as np
+import networkx as nx
+import sys
+sys.setrecursionlimit(15000)
 
 st.set_page_config(page_title="Scientific Computing", 
     page_icon="🧊", 
@@ -14,78 +19,77 @@ st.set_page_config(page_title="Scientific Computing",
 	initial_sidebar_state="collapsed", 
 	menu_items=None)
 
-
 # -----------
 # random walk
-def accumulate(x):
-    X=np.zeros(len(x))
-    X[0] = x[0]
-    
-    for i, _ in enumerate(x):
-        #st.write(i)
-        X[i] = X[i-1]+x[i]
-    return X
-
-def randomWalk(nsteps,
-                sigma2=1, seed=42,
-                xscale='linear', yscale='log'
-                ):
-    (dx_f, dy_f) = (lambda theta, r=1: r*trig(theta) for trig in (np.cos, np.sin)) 
-    dx_f = lambda theta, r = 1: r*np.cos(theta)
-    dy_f = lambda theta, r = 1: r*np.sin(theta)
-
-    np.random.seed(seed)
-    thetas_uniform = np.random.uniform(0,2*np.pi,nsteps)
-    thetas_randn = np.random.randn(nsteps)*sigma2
-    thetas_bimodal = np.concatenate([ np.random.randn(nsteps//2) * sigma2-1, 
-                                      np.random.randn(nsteps//2) * sigma2+1 ])
-
-    thetas_uniform[0] = 0
-    thetas_randn[0] = 0
-    thetas_bimodal[0] = 0
-
-    rands = [thetas_uniform, thetas_randn, thetas_bimodal]
-    rands_names = 'uniform, normal, bimodal'.split(', ')
-
-    def plot2():
-        colors = 'r g y'.split()
-        fig = plt.figure(figsize=(12,6))
-        
-        gs = GridSpec(3, 3, figure=fig)
-        ax1 = [fig.add_subplot(gs[i, 0]) for i in range(3)]
-        ax2 = fig.add_subplot(gs[:, 1:])    
-        
-        lims = {'xl':0, 'xh':0, 'yl':0, 'yh':0}
-        for i, (r, n) in enumerate(zip(rands, rands_names)):
-            dx, dy = dx_f(r), dy_f(r)
-            dx = np.vstack([np.zeros(len(dx)), dx]).T.flatten()
-            dy = np.vstack([np.zeros(len(dy)), dy]).T.flatten()
-            
-            ax1[i].plot(dx,dy, lw=1, c=colors[i])
-            ax1[i].set(ylim=(-1,1), xlim=(-1,1), 
-                        xticks=[], yticks=[],)
-            
-            x = accumulate(dx)
-            y = accumulate(dy)
-
-
-            ax2.plot(x,y, lw=2, label=n,c=colors[i])
-
-            ax1[i].set(facecolor = "black", xticks=[], yticks=[], )
-            ax2.set(facecolor = "black", xticks=[], yticks=[],xscale=xscale,yscale=yscale)
-            ax2.legend(fontsize=20)
-            
-
-            
-        plt.tight_layout()
-
-        ax1[0].set_title('steps', fontsize=24)
-        ax2.set_title('Cummulative', fontsize=24)
-        plt.tight_layout()
-        st.pyplot(fig)
-    plot2()
-
 def run_random_walk():
+    def accumulate(x):
+        X=np.zeros(len(x))
+        X[0] = x[0]
+        
+        for i, _ in enumerate(x):
+            #st.write(i)
+            X[i] = X[i-1]+x[i]
+        return X
+
+    def randomWalk(nsteps,
+                    sigma2=1, seed=42,
+                    xscale='linear', yscale='log'
+                    ):
+        (dx_f, dy_f) = (lambda theta, r=1: r*trig(theta) for trig in (np.cos, np.sin)) 
+        dx_f = lambda theta, r = 1: r*np.cos(theta)
+        dy_f = lambda theta, r = 1: r*np.sin(theta)
+
+        np.random.seed(seed)
+        thetas_uniform = np.random.uniform(0,2*np.pi,nsteps)
+        thetas_randn = np.random.randn(nsteps)*sigma2
+        thetas_bimodal = np.concatenate([ np.random.randn(nsteps//2) * sigma2-1, 
+                                          np.random.randn(nsteps//2) * sigma2+1 ])
+
+        thetas_uniform[0] = 0
+        thetas_randn[0] = 0
+        thetas_bimodal[0] = 0
+
+        rands = [thetas_uniform, thetas_randn, thetas_bimodal]
+        rands_names = 'uniform, normal, bimodal'.split(', ')
+
+        def plot2():
+            colors = 'r g y'.split()
+            fig = plt.figure(figsize=(12,6))
+            
+            gs = GridSpec(3, 3, figure=fig)
+            ax1 = [fig.add_subplot(gs[i, 0]) for i in range(3)]
+            ax2 = fig.add_subplot(gs[:, 1:])    
+            
+            lims = {'xl':0, 'xh':0, 'yl':0, 'yh':0}
+            for i, (r, n) in enumerate(zip(rands, rands_names)):
+                dx, dy = dx_f(r), dy_f(r)
+                dx = np.vstack([np.zeros(len(dx)), dx]).T.flatten()
+                dy = np.vstack([np.zeros(len(dy)), dy]).T.flatten()
+                
+                ax1[i].plot(dx,dy, lw=1, c=colors[i])
+                ax1[i].set(ylim=(-1,1), xlim=(-1,1), 
+                            xticks=[], yticks=[],)
+                
+                x = accumulate(dx)
+                y = accumulate(dy)
+
+
+                ax2.plot(x,y, lw=2, label=n,c=colors[i])
+
+                ax1[i].set(facecolor = "black", xticks=[], yticks=[], )
+                ax2.set(facecolor = "black", xticks=[], yticks=[],xscale=xscale,yscale=yscale)
+                ax2.legend(fontsize=20)
+                
+
+                
+            plt.tight_layout()
+
+            ax1[0].set_title('steps', fontsize=24)
+            ax2.set_title('Cummulative', fontsize=24)
+            plt.tight_layout()
+            st.pyplot(fig)
+        plot2()
+
     nsteps = st.sidebar.slider('nsteps',5,100)
     seed = st.sidebar.slider('seed',0,100)
     sigma2 = st.sidebar.slider('$\simga^2$',0.2,1.)
@@ -207,7 +211,7 @@ def mandelbroth():
         st.pyplot(fig)
 
     with st.sidebar:
-        logsize = st.slider('log(size)',1.5,3.)
+        logsize = st.slider('log(size)',1.5,4.)
         size = int(10**logsize)
         st.write(size)
         a = st.slider('a',0.01,13.,2.)
@@ -253,7 +257,8 @@ def bereaucrats():
     #plt.close()
     st.pyplot(fig)
     
-
+# -----------
+# bakSneppen
 def bakSneppen():
 
     def run(size, nsteps):
@@ -285,12 +290,53 @@ def bakSneppen():
     if run_=="yes":
         run(size, nsteps)
 
+# -----------
+# networkGenerator
+def network():
+    def make_network(n_persons = 5,alpha=.4):
+        
+        A = np.zeros((n_persons,n_persons))
+        for i in range(n_persons):
+            neighbours =  np.random.rand(n_persons)>alpha ; neighbours[i]=0
+            if sum(neighbours) == 0: 
+                a = np.random.randint(0,n_persons,4)
+                a = a[a!=i][0]
+                neighbours[a] =1
+            A[i] += neighbours; A[:,i] = neighbours
+        
+        return A
+
+    def draw_from_matrix(M, sick=[], pos=[]):
+        sick = np.zeros(len(M)) if len(sick) == 0 else sick
+        G = nx.Graph()
+        for i, line in enumerate(M):
+            G.add_node(i)
+
+        for i, line in enumerate(M):
+            for j, val in enumerate(line):
+                if (i != j) and (val==1): 
+                    G.add_edge(i, j)
+        color_map = ['r' if s==1 else 'b' for s in sick]
+        
+        pos = nx.nx_agraph.graphviz_layout(G) if len(pos)==0 else pos
+        
+        nx.draw_networkx(G,pos, node_color=color_map)
+        return pos
 
 
+    with st.sidebar:
+        N = st.slider('N',1,42,22)
+        a = st.slider('alpha', 0.,1.,0.97)
+    fig, ax = plt.subplots()
 
-viz = st.sidebar.selectbox('viz', ['choose', 'RandomWalk 2d', 
+    net = make_network(N,a)
+    draw_from_matrix(net)
+    st.pyplot(fig)
+
+viz = st.sidebar.selectbox('viz', ['RandomWalk 2d', 
                                     'Percolation', 'mandelbroth',
-                                    'bereaucrats', 'bakSneppen'])
+                                    'bereaucrats', 'bakSneppen',
+                                    'network'])
 if viz =='RandomWalk 2d':
     run_random_walk()
 elif viz =='Percolation':
@@ -299,9 +345,10 @@ elif viz =='mandelbroth':
     mandelbroth()
 elif viz =='bereaucrats':
     bereaucrats()
-
 elif viz =='bakSneppen':
     bakSneppen()
+elif viz == 'network':
+    network()
 
 
 
