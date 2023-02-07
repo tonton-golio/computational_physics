f26cfb93420ef0e1b4a098afc76c2cbf895d9293
MBach0707 <michaelbach0707@gmail.com>
2023-02-07 20:23:08 +0100

    Corrected text

--- pages/continuum_mechanics.py
+++ pages/continuum_mechanics.py
@@ -5,12 +5,12 @@ set_rcParams()
 
 def frontpage():
     ''
-    """# Continuum Mechanics"""
+    st.title("""Continuum Mechanics""")
     #text_dict = getText_prep(filename = text_path+'landingPage.md', split_level = 1)
     #for key in text_dict:
     #    text_dict[key]
 
-    """
+    st.write("""
     In the macroscopic world, most materials that surround us e.g. solids and 
     liquids can safely be assumed to exist as continua, that is, the materials 
     completely fill the space they occupy and the underlying atomic structures 
@@ -30,29 +30,28 @@ def frontpage():
     numerical modeling of continous matter will be given.
     
     From: https://kurser.ku.dk/course/nfyk10005u
-    """
+    """)
 
     cols = st.columns(3)
-    cols[0].image('https://nci-media.cancer.gov/pdq/media/images/428431-571.jpg')
-    cols[0].caption('magnetic resonance imaging (MRI)')
+    cols[0].image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FVortex&psig=AOvVaw1nOAFgaJ6XOZyXfZkmfmac&ust=1675880067257000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOiA4_KBhP0CFQAAAAAdAAAAABAR')
+    cols[0].caption('Vortex around an airplane')
 
-    cols[1].image('https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Amadeo_Avogadro.png/248px-Amadeo_Avogadro.png')
-    cols[1].caption('Amedeo Avogadro.')
+    cols[1].image('https://i.pinimg.com/originals/49/f3/94/49f3946a610b9a3665c9e2c9bd4c571c.jpg')
+    cols[1].caption('')
 
+    cols[2].image('https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Amadeo_Avogadro.png/248px-Amadeo_Avogadro.png')
+    cols[2].caption('Amedeo Avogadro')
 
-    cols[2].image('https://i.pinimg.com/originals/49/f3/94/49f3946a610b9a3665c9e2c9bd4c571c.jpg')
-    cols[2].caption('')
-
-    '''
-    Navigate between the follwoing topics in the left menu.
-    1. Criteria for Continua
+    st.title("""Table of contents""")
+    st.write("""
+    1. The Continuum Approximation
     2. Elasticity
     3. Finite element modelling
     4. Continuum Dynamics and Ideal flows
     5. Viscosity and Gravity waves
     6. Pipe flow
     7. Navier Stokes Equation
-    '''
+    """)
 
 def testpage():
     ''
@@ -79,15 +78,12 @@ def testpage():
 
 def contapprox():
     ''
-    cols = st.columns(2)
-    cols[0].title('The Continuum Approximation')
-    
-    # intro 
-    cols[0].write(r"""
+    st.title('The Continuum Approximation')
+    st.write(r"""
     The physics of continuum mechanics, requires that a mass of particles can be 
     modelled as continuous matter where the particles are infinitesimal. 
     
-    \textit{
+    $\textit{
     "Whether a given number of molecules is large enough to warrant the use of 
     a smooth continuum description of matter depends on the desired precision. 
     Since matter is never continuous at sufficiently high precision, continuum 
@@ -97,15 +93,14 @@ def contapprox():
     continuity, one must so to speak avoid looking too sharply at material bodies. 
     Fontenelle stated in a similar context that 
     "Science originates from curiosity and bad eyesight"."
-    }
+    }$
     
     From Physics of Continuous Matter 2nd edition by Benny Lautrup
     
     """)
-    cols[1].image('https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Amadeo_Avogadro.png/248px-Amadeo_Avogadro.png', 
-            width=300, caption='Avada Avogadro')
+    
+    st.title('Gases')
     st.write(r"""
-    The Continuum Approximation for Gases
     
     Density fluctuations
     
@@ -182,8 +177,8 @@ def contapprox():
 # Navigator
 topic_dict = {
     "Frontpage" : frontpage,
-    'Testpage'  : testpage,
     'The Continuum Approximation' : contapprox,
+    'Testpage'  : testpage,
               }
 
 topic = st.sidebar.selectbox("topic" , list(topic_dict.keys()))
