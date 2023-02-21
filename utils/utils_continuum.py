20ddcacdac4c63478e34094e1063c3ee12ac02cb
MBach0707 <michaelbach0707@gmail.com>
2023-02-21 23:01:45 +0100

    s

--- utils/utils_continuum.py
+++ utils/utils_continuum.py
@@ -1,5 +1,8 @@
 from utils.utils_global import *
-import yfinance as yf
-import networkx as nx # having trouble with this when hosted
 
 textfile_path = 'assets/continuum_mechanics/text/'
+imagefile_path = 'assets/continuum_mechanics/images/'
+
+signatur = """*If you want to contribute, report errors, have feedback or comments, kindly contact*
+    
+    Michael Bach - Discord: bach#2630 - lvp115@alumni.ku.dk"""
