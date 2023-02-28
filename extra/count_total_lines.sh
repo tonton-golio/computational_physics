7170cbcee06d95099cda9d350b9af2e5cbec3e02
Anton Gersvang Golles <aggolles97@gmail.com>
2023-02-28 21:08:48 +0100

    move extra files to extra

---
+++ extra/count_total_lines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+echo "I'm gonna count up all the lines for you <3, give me two seconds"
+sleep 1
+cd ..
+echo
+echo 'python'
+git ls-files | grep -e '.py'|xargs wc -l
+
+echo
+echo
+echo 'markdown'
+git ls-files | grep -e '.md'|xargs wc -l
+
