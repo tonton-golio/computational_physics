9e4a0d025bfa5992d75907e215d8b9ad017b017b
Anton Gersvang Golles <62528977+tonton-golio@users.noreply.github.com>
2023-03-02 13:31:17 +0100

    Update home.py

--- home.py
+++ home.py
@@ -7,12 +7,8 @@ st.set_page_config(page_title='Computational Physics',#"plz contribute",
 	menu_items=None)
 st.markdown(r"""
 	# Computational Physics
-	*by*: Anton Gersvang Golles, Riz Fernando Noronha & Yoshiaki Horiike.
+	*by*: Anton Gersvang Golles, Riz Fernando Noronha, Yoshiaki Horiike & Michael Bach.
 
 	Please contribute on [github](https://www.github.com/tonton-golio/computational_physics)
 	""")
 st.image('assets/images/mandel_front.png', width=420)
-
-import cProfile
-import re
-cProfile.run('re.compile("foo|bar")')
