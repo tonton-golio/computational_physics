70f3da8040ed9170cdce212b0f89985c0e00c250
Anton Gersvang Golles <aggolles97@gmail.com>
2023-03-05 21:28:21 +0100

    updated search, still needs to access the navigator function

--- pages/graf.py
+++ pages/graf.py
@@ -6,54 +6,86 @@ import re
 import pandas as pd
 
 
-textdict = dict()
-counter = 0
+def get_tree(os_ = "windows", entensions = ['md','txt']):
 
-for root, dirs, files in os.walk('.'):
-    if '.\\assets' in root or '.\\pages' in root:
-        for file in files:
-            extension = file.split('.')[1]
-            if extension in ['md','txt']:
-                with open(root + '\\' + file,'rt', encoding='utf-8') as f:
-                    textdict[counter] = {
-                        'root' : root,
-                        'filename' : root + '\\'+file,
-                        'content' : f.read(),
-                        }
-                    counter += 1
-                    
-#st.write(textdict)
-searchterm = st.text_input(label = 'Search')
-st.write(searchterm)
-if searchterm == '':
-    pass
-elif len(searchterm) < 3:
-    pass
-else: 
+    textdict = dict()
+    counter = 0
+
+    if os_ == "windows":
+        dir_sep = "\\"
+    elif os_ == "linux":
+        dir_sep = "/"
+
+    dir_sep = {
+        "windows" : "\\",
+        "linux" : "/"
+    }[os_]
+    for root, dirs, files in os.walk('.'):
+        if '.'+dir_sep+'assets' in root or '.'+dir_sep+'pages' in root:
+            for file in files:
+                
+                extension = file.split('.')[1]
+                if extension in entensions:
+                    with open(root + dir_sep + file,'rt', encoding='utf-8') as f:
+
+                        textdict[counter] = {
+                            'root' : root,
+                            'filename' : root + dir_sep+file,
+                            'content' : f.read(),
+                            }
+                        counter += 1
+
+    return textdict
+    
+def search(searchterm, tree):
+    assert type(searchterm) == str
+    assert len(searchterm) > 2
+ 
     p = re.compile(searchterm, re.IGNORECASE)
     matches = dict()
 
-    for i in textdict:
-        test = p.findall(textdict[i]['content'])
-        if test == []:
-            pass
-        else:
-            matches[textdict[i]['filename']] = len(test)
+    for i in tree:
+        find_all = p.findall(tree[i]['content'])
+        if find_all == []:  pass
+        else:  matches[tree[i]['filename']] = len(find_all)
+
+    
     matches = [(key,val) for key, val in matches.items()]
     df = pd.DataFrame(matches)
     df.sort_values(by=[1], ascending=False, inplace = True)#.head(5)
     
-    topresults = df.head(5)[0]
-    filepath = topresults[0]
-    st.write(topresults)
-"""
-for root, dirs, files in os.walk('.\\pages'):
-    for file in files:
-        file
-        extension = file.split('.')[1]
-        if extension in ['py']:
-            with open(root + '\\' + file,'rt', encoding='utf-8') as f:
-                st.write(f.read())
-                if filepath[2:] in f.read():
-                    filepath, file
-"""
+    topresults = df.head(5)[0].values
+    return topresults
+
+
+with st.sidebar:
+    tree_md = get_tree(os_="linux") 
+
+    searchterm = st.text_input(label = 'Search')
+    #st.write("searchterm = ", searchterm)
+    try:
+        topresults = search(searchterm, tree_md)
+        #st.write(topresults)
+        # formatting topresults
+        #subjects = [i.split('/')[-3] for i in topresults]
+        #subjects
+
+        topresults = {i.split('/')[-1].split('.')[0] : i for i in topresults}
+        filepath = topresults[st.selectbox(label = 'Select pages', options = topresults.keys())]
+        # get page
+
+
+
+        tree_py = get_tree(os_="linux", entensions = ['py']) 
+        searchterm = filepath.split('/')[-1]
+        #st.write("searchterm = ", searchterm)
+        topresults = search(searchterm, tree_py)
+        page = topresults[0].split('/')[-1].split('.')[0]
+        # navigate to file
+        link = 'https://physics.streamlit.app/'
+        st.markdown('[link to: {}]({})'.format(page, link+page))
+            
+
+    except:
+        st.write('Please enter a searchterm (at least 3 characters long)')
+
