ab7192582cc65bc4f652e98d96484dd6440a05f6
KoalaClaw <koalaclaw69@gmail.com>
2026-02-17 07:46:31 +0100

    Remove outdated TODO comments for implemented topics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-// TODO: Continuum Mechanics
-// TODO: Dynamical Models
-// TODO: Inverse Problems
-// TODO: Online Reinforcement
-// TODO: Quantum Optics
-// TODO: Scientific Computing
 import { redirect } from "next/navigation";
 
 // Redirect home to graph-based navigation
